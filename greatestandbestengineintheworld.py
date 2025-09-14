1f93260cebbf06a30b51c7cd5708c8ec35d3d01d
SolidLamp <138074663+SolidLamp@users.noreply.github.com>
2025-09-14 17:05:07 +0100

    Lambda functions.

--- greatestandbestengineintheworld.py
+++ greatestandbestengineintheworld.py
@@ -6,7 +6,22 @@ import shm
 
 print2 = shm.print2
 roomID = 1
+
+
 history = []
+inventory = ["Item","Golden Idol"]
+@dataclass
+class gameState:
+    inventory: inventory
+    rubies: int = 0
+    shopkeeperName: str = "The Shopkeeper"
+    position: float = 0.0
+    beatenGame: bool = False
+    caveOpened: bool = False
+    fletcherOpen: bool = True
+
+game_state = gameState(inventory=inventory)
+
 
 def option(choice, Inventory=True):
     print()
@@ -23,36 +38,66 @@ def option(choice, Inventory=True):
 
 
 rooms = {
+    0: {
+        "Text": "This is the Debug Room.",
+        "Options": ["Go to 2", "Go to 3"],
+        "Move": [2,3],
+    },
     1: {
         "Text": "This is the First Room.",
         "Options": ["Go to 2", "Go to 3"],
-        "Move": [2,3]
+        "Move": [2,3],
     },
     2: {
-        "Text": "This is the Second Room.",
+        "Text": "You are within the cave. It is a dead-end. The only path is back where you came.",
+        "AlternateText": "You are within the cave. It is a dead-end. The only path is back where you came.",
+        "Item": "Golden Idol",
+        "ItemRequirement": lambda: "Golden Idol" in inventory,
+        "ItemText": "You see, on the floor of the cave, a \033[47mGolden Idol\033[0m.",
         "Options": ["Go to 1", "Go to 3"],
-        "Move": [1,3]
+        "Move": [1,3],
     },
     3: {
-        "Text": "This is the Third Room.",
-        "Options": ["Go to 1", "Go to 2"],
-        "Move": [1,2]
+        "Text": "You are within the cave. It is difficult to see. There is a path to your northeast, south and northwest.",
+        "Requirements": lambda: game_state.caveOpened,
+        "AlternateText": "You are within the cave. It is difficult to see. There is a path to your northeast and south. There is a lot of rubble to your northwest.",
+        "ItemText": "You see, on the floor of the cave, a \033[47mGolden Idol\033[0m.",
+        "Options": ["Go to 1", "Go to 4","Go to 2"],
+        #"Option1Requirements": lambda: not game_state.caveOpened,
+        "Option2Requirements": lambda: game_state.caveOpened,
+        "Move": [1,4,2],
+    },
+    4: {
+        "Text": "You opened the cave.",
+        "Script": lambda: setattr(game_state, "caveOpened", True),
+        "Automove": 3,
     },
 }
 
 def gameLoop(): 
     global roomID
-    print(rooms[roomID]["Move"]) 
-    query = option(rooms[roomID]["Options"])
-    print(len(rooms[roomID]["Move"]))
-    if query.isdigit() and int(query) <= len(rooms[roomID]["Move"]):
-        query = int(query) - 1
-        print("Valid")
-        roomID = rooms[roomID]["Move"][query]
-        history.append(roomID)
-        if len(history) > 5:
-            history.pop(0)
-        print(roomID)
+    room = rooms[roomID]
+    if "Requirements" in room and not room["Requirements"]():
+        print2(room["AlternateText"])
+    else:
+        print2(room["Text"])
+    if "Automove" in room:
+        roomID = room["Automove"]
+    else:
+        Options = []
+        for i in range(len(room["Move"])):
+            print(i)
+            OptionRequirements = "Option" + str(i) + "Requirements"
+            if OptionRequirements not in room or room[OptionRequirements]():
+                Options.append(room["Move"][i])
+        query = option(Options)
+        if query.isdigit() and int(query) <= len(room["Move"]):
+            roomID = room["Move"][int(query) - 1]
+    if "Script" in room:
+        room["Script"]()
+    history.append(roomID)
+    if len(history) > 5:
+        history.pop(0)
 
 def test():
     global roomID
@@ -65,17 +110,9 @@ def test():
         print2("WHAT?")
         action1()
 
-print(rooms[roomID]["Move"]) 
-query = option(rooms[1]["Options"])
-print(len(rooms[roomID]["Move"]))
-if query.isdigit() and int(query) <= len(rooms[roomID]["Move"]):
-    query = int(query) - 1
-    print("Valid")
-    roomID = rooms[roomID]["Move"][query]
-    history.append(roomID)
-    if len(history) > 5:
-        history.pop(0)
-    print(roomID)
+
+
+
 
 while True:
     gameLoop()
