a4967cb1be88a92dfd916636da15084bb212ba09
SolidLamp <138074663+SolidLamp@users.noreply.github.com>
2026-02-25 22:56:48 +0000

    [Refactor] Improve code of save_handler.py

--- src/save_handler.py
+++ src/save_handler.py
@@ -5,21 +5,70 @@ import shutil
 import uuid
 
 
+def save_validifier(saveFile: dict) -> bool:
+    if not isinstance(saveFile, dict):
+        return False
+    if "Game" not in saveFile or not isinstance(saveFile["Game"], str):
+        return False
+    if "game_state" not in saveFile or not isinstance(saveFile["game_state"], dict):
+        return False
+    if "RoomID" not in saveFile or not isinstance(saveFile["RoomID"], int):
+        return False
+    if "History" not in saveFile or not isinstance(saveFile["History"], list):
+        return False
+    return True
+
+
+def remove_extension(file_name: str) -> str:
+    file_name.removesuffix(".json")
+    file_name.removesuffix(".save")
+    file_name.removesuffix(".sav")
+    return file_name
+
+
+def read_json(file_name: str = "file.json") -> dict:
+    """Reads json from a the file `file_name`"""
+    if not os.path.exists(fileName):
+        return {}
+    else:
+        with open(fileName, "rt") as f:
+            gamedata = json.load(f).copy()
+        return gamedata
+
+
+def read_save(file_name: str = "game") -> dict:
+    """
+    Reads the save from {game}.sav.
+    fileName should not include the .sav file extension
+    """
+    file_name = remove_extension(file_name)
+    file_name += ".sav"
+    gamedata = read_json(file_name)
+    return gamedata
+
+
+def write_json(saveFile: str, fileName: str = "game.sav") -> None:
+    """fileName should include the file extension"""
+    with open(fileName, "wt") as f:
+        f.write(saveFile)
+
+
 def write_save(
     game_state,
     gameInfo: dict,
     room: int,
     history: list[int],
-    fileName: str = "game",
+    file_name: str = "game",
     save_version: int = 0,
-    game_id: str | uuid.UUID | uuid.SafeUUID | None = None,
-    save_id: str | uuid.UUID | uuid.SafeUUID | None = None,
+    game_id: str | uuid.UUID | None = None,
+    save_id: str | uuid.UUID | None = None,
 ) -> None:
-    """fileName should not include the .sav file extension"""
-    existing_save = read_save(fileName)
+    """file_name should not include the .sav file extension"""
+    file_name = remove_extension(file_name)
+    existing_save = read_save(file_name)
     if existing_save and existing_save.get("save_id", "2") != save_id:
-        copy_save(fileName, existing_save)
-    fileName = fileName + ".sav"
+        copy_save(file_name, existing_save)
+    file_name += ".sav"
     dictionary = {
         "Game": gameInfo["title"],
         "Saved": datetime.now().isoformat(),
@@ -37,45 +86,14 @@ def write_save(
         dictionary.update({"save_id": save_id})
     else:
         dictionary.update({"save_id": str(uuid.uuid4())})
-        # e.g. 164237f8-d3ce-46b2-95b1-b30d39ce4472
     output = json.dumps(dictionary, indent=4)
     write_json(output, fileName)
 
 
-def write_json(saveFile: str, fileName: str = "game.sav") -> None:
-    """fileName should include the file extension"""
-    with open(fileName, "wt") as f:
-        f.write(saveFile)
-
-
-def read_save(gameName: str = "game") -> dict:
-    """fileName should not include the .sav file extension"""
-    fileName = gameName + ".sav"
-    if not os.path.exists(fileName):
-        return {}
-    else:
-        with open(fileName, "rt") as f:
-            gamedata = json.load(f).copy()
-        return gamedata
-
-
-def save_validifier(saveFile: dict) -> bool:
-    if not isinstance(saveFile, dict):
-        return False
-    if "Game" not in saveFile or not isinstance(saveFile["Game"], str):
-        return False
-    if "game_state" not in saveFile or not isinstance(saveFile["game_state"], dict):
-        return False
-    if "RoomID" not in saveFile or not isinstance(saveFile["RoomID"], int):
-        return False
-    if "History" not in saveFile or not isinstance(saveFile["History"], list):
-        return False
-    return True
-
-
 def copy_save(file_name: str, existing_save: dict) -> None:
+    file_name = remove_extension(file_name)
     old_save = os.path.abspath(file_name + ".sav")
     old_copy = (
-        os.path.abspath(file_name) + existing_save.get("Saved", " - Copy") + ".sav"
+        os.path.abspath(file_name) + "-" + existing_save.get("Saved", "copy") + ".sav"
     )
     shutil.copyfile(old_save, old_copy)
