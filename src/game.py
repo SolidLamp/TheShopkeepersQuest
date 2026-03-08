0d89cc7ecb44fb54f9573bc6bf4f21cbe805bdaa
SolidLamp <138074663+SolidLamp@users.noreply.github.com>
2026-03-08 15:24:07 +0000

    [Refactor] Improve the readability of game.py

--- src/game.py
+++ src/game.py
@@ -9,59 +9,58 @@ import toml_reader
 
 print3 = tui.print3
 
-gameInfo = toml_reader.read_toml("game.toml")["game_info"]
 
-history = []
+gameInfo: dict[str, Any]
+history: dict[int]
+endingText: dict[str, str]
+defaultEnding: str
+loseText: dict[str, str]
+defaultLose: str
+keyItems: list[str]
 
-ShopkeeperQuotesExit = [
-    "Remember, tomorrow is another day.",
-    "Be seeing you.",
-    "I wish you the best of luck.",
-    "I give you my grace.",
-    "I wish you luck on your quest.",
-    "I'll get started on my tea.",
-    "May all the spirits be with you.",
-]
 
-ShopkeeperQuotes = [
-    "Welcome!",
-    "Sit down, my friend. Stay a while.",
-    "You'll always be welcome here in my shop.",
-    "Have a look at my wares. They could prove to be useful.",
-    "Sorry I can't give discounts - I have a business to run, and I must make my money somehow.",
-    "I don't blame you for coming inside in this winter.",
-    "Sit down a while. I don't want you too stressed, my friend.",
-    "I always believe that everyone has a tale worth telling - maybe talk to more people.",
-    "Have you checked out the bazaar? There's a fisherman there who sells excellent-quality rods.",
-    "Have you heard the news about the King? It's said that he's fallen ill. He's a good man - I hope he fully recovers.",
-    "Have you seen the nearby forest? There's been some monkey sightings there.",
-]
+gameInfo: dict[str, Any] = toml_reader.read_toml("game.toml")["game_info"]
 
-endingText = {
-    "Good": "And so, overnight, all the people returned to the village, as if they had never left.\nSoon after, the village was lifted into high spirits as the harvest had been the best in almost thirty years.\nDespite the prospering village, you decided to leave.\nYou had no desire to stay after the events you just experienced, and you would rather leave than stay to make some money.",
-    "Secret": "And so, overnight, you became the new shopkeeper, but nothing really changed in the end.\nThe villagers never returned, but many travellers came, hearing about what happened.\nMany decided to stay after a plentiful harvest brought good omens to the village.\nThis, however, would not be the last of it...\n...and you knew that.",
-    "SHM": "You achieved the\n|\nEnding.\nTry Again?",
-}
-defaultEnding = "\033[1mThe Shopkeeper's Quest\033[0m\n1.2\n\nCreated by SolidLamp\n\nWith inspiration from:\nColossal Cave Adventure, by Will Crowther and Don Woods;\nKing's Quest, by Sierra On-Line;\nHenry Stickmin, by Puffballs United;\nMinecraft: Story Mode, by Telltale Games;\nand RTX Morshu: The Game, by koshkamatew\nWith special thanks to\n\033[1m\033[33mYOU\033[0m\nfor playing the game,\nfor if a tree falls and no one hears it, does it make a noise?"
+history: list[int] = []
 
-loseText = {
+endingText: dict[str, str] = {
+    "Good": "And so, overnight, all the people returned to the village, as "
+        "if they had never left.\nSoon after, the village was lifted into high "
+        "spirits as the harvest had been the best in almost thirty years.\n"
+        "Despite the prospering village, you decided to leave.\nYou had no "
+        "desire to stay after the events you just experienced, and you would "
+        "rather leave than stay to make some money.",
+    "Secret": "And so, overnight, you became the new shopkeeper, but nothing "
+        "really changed in the end.\nThe villagers never returned, but many "
+        "travellers came, hearing about what happened.\nMany decided to stay "
+        "after a plentiful harvest brought good omens to the village.\nThis, "
+        "however, would not be the last of it...\n...and you knew that.",
     "SHM": "You achieved the\n|\nEnding.\nTry Again?",
 }
 
-defaultLose = "\n\n\033[31m\033[1mYou died!\033[0m\n'|'\n\n\n"
+defaultEnding: str = (
+    "\033[1mThe Shopkeeper's Quest\033[0m\n"
+    "1.2\n\n"
+    "Created by SolidLamp\n\n"
+    "With inspiration from:\n"
+    "Colossal Cave Adventure, by Will Crowther and Don Woods;\n"
+    "King's Quest, by Sierra On-Line;\n"
+    "Henry Stickmin, by Puffballs United;\n"
+    "Minecraft: Story Mode, by Telltale Games;\n"
+    "and RTX Morshu: The Game, by koshkamatew\n"
+    "With special thanks to\n"
+    "\033[1m\033[33mYOU\033[0m\n"
+    "for playing the game,\n"
+    "for if a tree falls and no one hears it, does it make a sound?"
+)
 
-mysticalRocks = {
-    "What you think is an Emerald": 80,
-    "A Funny-looking Rock": 5,
-    "A Chunk of Marble": 15,
-    "Some Tourmaline, maybe": 50,
-    "A Stone that looks kind of like a face": 150,
-    "A Weird shard of something": 20,
-    "Some strange stone": 100,
-    "A beautiful, azure blue rock": 500,
+loseText: dict[str, str] = {
+    "SHM": "You achieved the\n|\nEnding.\nTry Again?",
 }
 
-keyItems = [
+defaultLose: str = "\n\n\033[31m\033[1mYou died!\033[0m\n'|'\n\n\n"
+
+keyItems: list[str] = [
     "Rusted Sword",
     "Amber Necklace",
     "Golden Idol",
@@ -150,88 +149,6 @@ class gameState:
 game_state = gameState(inventory=Inventory(items=[]))
 
 
-def itemEvaluation() -> int:
-    cursedItems = len(
-        {"Rusted Sword", "Amber Necklace", "Golden Idol"}
-        & set(game_state.inventory.keyItems)
-    )
-    return cursedItems
-
-
-def ShopkeeperFinalSpeech(win: curses.window) -> None:
-    print3(
-        win,
-        "\033[33m'Well, I'll be. That's all of them. Honestly, I kind of doubted you could do it - now I see that my doubt was misplaced! You will go down in legend for your heroism!'",
-    )
-    time.sleep(0.75)
-    print3(win, "\033[33m\n'Oh, and just one more thing: thank you.'\033[0m")
-    time.sleep(1.5)
-
-
-def ShopkeeperAffirmations(win: curses.window) -> None:
-    cursedItems = len(
-        {"Rusted Sword", "Amber Necklace", "Golden Idol"}
-        & set(game_state.inventory.keyItems)
-    )
-    print3(
-        win,
-        f"\033[33m'Great! You managed to get {cursedItems} of the items - now we just need {3 - cursedItems} more!'\033[0m",
-    )
-
-
-def fishEvaluation(win: curses.window) -> None:
-    fishBought = len(
-        {
-            "Fillet of Cod",
-            "Fillet of Salmon",
-            "Smoked Trout",
-            "Jar of Tuna",
-            "Fillet of Smoked Haddock",
-        }
-        & set(game_state.inventory.keyItems)
-    )
-    if fishBought >= 1 and not game_state.inventory.hasItem("Fishing Rod"):
-        print3(
-            win,
-            "\033[36m'You know, I recently got a new fishing rod. Say, you can have my old one, since you bought something.'\033[0m",
-        )
-        game_state.inventory.getKeyItem("Fishing Rod", win)
-    else:
-        print3(
-            win,
-            "\033[36m'You know, I'd be more in the mood to talk if you bought something.'\033[0m",
-        )
-
-
-def mineralEvaluation(win: curses.window) -> None:
-    for item in [
-        item for item in list(game_state.inventory.items) if item in list(mysticalRocks)
-    ]:  # This is not that readable but it does stuff basically]
-        value = mysticalRocks[item]
-        game_state.getRuby(value, win)
-        game_state.inventory.items.remove(item)
-    print3(
-        win,
-        "\033[32m'Thank you so much. These will be excellent to add to my collection.'\033[0m",
-    )
-
-
-def debug(win: curses.window) -> None:
-    game_state.inventory.getKeyItem("Rusted Sword", win)
-    game_state.inventory.getKeyItem("Amber Necklace", win)
-    game_state.inventory.getKeyItem("Golden Idol", win)
-    game_state.inventory.getKeyItem("Ancient Key", win)
-    game_state.inventory.getKeyItem("Fishing Rod", win)
-    game_state.inventory.getKeyItem("Bow and Arrow", win)
-    game_state.inventory.getKeyItem("Silver Amulet", win)
-    game_state.inventory.getItem("Lamp Oil", win)
-    game_state.inventory.getItem("Lamp Oil", win)
-    game_state.inventory.getItem("Rope", win)
-    game_state.inventory.getItem("Bomb", win)
-    game_state.inventory.getItem("Bomb", win)
-    game_state.getRuby(9001, win)
-
-
 def overwrite_rooms(rooms: dict, extrarooms: dict) -> dict:
     new_rooms = rooms.copy()
     for key in filter(lambda key: key in rooms.keys(), extrarooms.keys()):  # orubt
@@ -286,7 +203,7 @@ def get_rooms(win: curses.window) -> dict:
             "KeyItemRequirements": lambda: not hasItem("Amber Necklace"),
         },
         66: {
-            "Text": "\033[33m'"
+            "Text": "\033[93m'"
             + random.choice(ShopkeeperQuotes)
             + "'\033[0m\nCurrent Rubies: "
             + str(game_state.rubies)
@@ -315,16 +232,19 @@ def get_rooms(win: curses.window) -> dict:
             ),
         },
         71: {
-            "Text": "\033[33m'" + random.choice(ShopkeeperQuotesExit) + "'\033[0m",
+            "Text": "\033[93m'" + random.choice(ShopkeeperQuotesExit) + "'\033[0m",
         },
         73: {
             "TextRequirements": lambda: game_state.fletcherOpen,
-            "Option1Requirements": lambda: game_state.fletcherOpen
+            "Option1Requirements": lambda: game_state.fletcherOpen,
         },
         74: {
             "Text": "Current Rubies:"
             + str(game_state.rubies)
-            + "\nFillet of Cod - 2 rubies\nFillet of Salmon - 2 rubies\nSmoked Trout - 9 rubies\nJar of Tuna - 3 rubies\nFillet of Smoked Haddock - 6 rubies\n\033[36m'What are you here to buy?'\033[0m",
+            + "\nFillet of Cod - 2 rubies\nFillet of Salmon - 2 rubies\n"
+            + "Smoked Trout - 9 rubies\nJar of Tuna - 3 rubies\n"
+            + "Fillet of Smoked Haddock - 6 rubies\n"
+            + "\033[36m'What are you here to buy?'\033[0m",
             "Option0Requirements": lambda: game_state.rubies >= 2,
             "Option1Requirements": lambda: game_state.rubies >= 2,
             "Option2Requirements": lambda: game_state.rubies >= 9,
@@ -422,7 +342,8 @@ def get_rooms(win: curses.window) -> dict:
             and random.randrange(0, 3) == "2",
         },
         114: {
-            "Text": "You are within the cave. It is difficult to see. There is a path to your northeast and northwest.",
+            "Text": "You are within the cave. It is difficult to see. "
+                    "There is a path to your northeast and northwest.",
             "Item": "A Weird shard of something",
             "ItemRequirements": lambda: not hasItem("A Weird shard of something")
             and random.randrange(0, 3) == "2",
@@ -459,4 +380,132 @@ def get_rooms(win: curses.window) -> dict:
     return rooms
 
 
-##
+# Additional Scripts
+
+
+ShopkeeperQuotesExit = [
+    "Remember, tomorrow is another day.",
+    "Be seeing you.",
+    "I wish you the best of luck.",
+    "I give you my grace.",
+    "I wish you luck on your quest.",
+    "I'll get started on my tea.",
+    "May all the spirits be with you.",
+]
+
+ShopkeeperQuotes = [
+    "Welcome!",
+    "Sit down, my friend. Stay a while.",
+    "You'll always be welcome here in my shop.",
+    "Have a look at my wares. They could prove to be useful.",
+    "Sorry I can't give discounts - I have a business to run, and I must make my "
+        "money somehow.",
+    "I don't blame you for coming inside in this winter.",
+    "Sit down a while. I don't want you too stressed, my friend.",
+    "I always believe that everyone has a tale worth telling - maybe talk to "
+        "more people.",
+    "Have you checked out the bazaar? There's a fisherman there who sells "
+        "excellent-quality rods.",
+    "Have you heard the news about the King? It's said that he's fallen ill. "
+        "He's a good man - I hope he fully recovers.",
+    "Have you seen the nearby forest? There's been some monkey sightings there.",
+]
+
+mysticalRocks = {
+    "What you think is an Emerald": 80,
+    "A Funny-looking Rock": 5,
+    "A Chunk of Marble": 15,
+    "Some Tourmaline, maybe": 50,
+    "A Stone that looks kind of like a face": 150,
+    "A Weird shard of something": 20,
+    "Some strange stone": 100,
+    "A beautiful, azure blue rock": 500,
+}
+
+
+def itemEvaluation() -> int:
+    cursedItems = len(
+        {"Rusted Sword", "Amber Necklace", "Golden Idol"}
+        & set(game_state.inventory.keyItems)
+    )
+    return cursedItems
+
+
+def ShopkeeperFinalSpeech(win: curses.window) -> None:
+    print3(
+        win,
+        "\033[93m'Well, I'll be. That's all of them. Honestly, I kind of "
+        "doubted you could do it - now I see that my doubt was misplaced! "
+        "You will go down in legend for your heroism!'",
+    )
+    time.sleep(0.75)
+    print3(win, "\033[93m\n'Oh, and just one more thing: thank you.'\033[0m")
+    time.sleep(1.5)
+
+
+def ShopkeeperAffirmations(win: curses.window) -> None:
+    cursedItems = len(
+        {"Rusted Sword", "Amber Necklace", "Golden Idol"}
+        & set(game_state.inventory.keyItems)
+    )
+    print3(
+        win,
+        f"\033[93m'Great! You managed to get {cursedItems} of the items - now "
+        f"we just need {3 - cursedItems} more!'\033[0m",
+    )
+
+
+def fishEvaluation(win: curses.window) -> None:
+    fishBought = len(
+        {
+            "Fillet of Cod",
+            "Fillet of Salmon",
+            "Smoked Trout",
+            "Jar of Tuna",
+            "Fillet of Smoked Haddock",
+        }
+        & set(game_state.inventory.keyItems)
+    )
+    if fishBought >= 1 and not game_state.inventory.hasItem("Fishing Rod"):
+        print3(
+            win,
+            "\033[36m'You know, I recently got a new fishing rod. Say, you "
+            "can have my old one, since you bought something.'\033[0m",
+        )
+        game_state.inventory.getKeyItem("Fishing Rod", win)
+    else:
+        print3(
+            win,
+            "\033[36m'You know, I'd be more in the mood to talk if you bought "
+            "something.'\033[0m",
+        )
+
+
+def mineralEvaluation(win: curses.window) -> None:
+    for item in [
+        item for item in list(game_state.inventory.items) if item in list(mysticalRocks)
+    ]:  # This is not that readable but it does stuff basically]
+        value = mysticalRocks[item]
+        game_state.getRuby(value, win)
+        game_state.inventory.items.remove(item)
+    print3(
+        win,
+        "\033[32m'Thank you so much. These will be excellent to add to my "
+        "collection.'\033[0m",
+    )
+
+
+def debug(win: curses.window) -> None:
+    game_state.inventory.getKeyItem("Rusted Sword", win)
+    game_state.inventory.getKeyItem("Amber Necklace", win)
+    game_state.inventory.getKeyItem("Golden Idol", win)
+    game_state.inventory.getKeyItem("Ancient Key", win)
+    game_state.inventory.getKeyItem("Fishing Rod", win)
+    game_state.inventory.getKeyItem("Bow and Arrow", win)
+    game_state.inventory.getKeyItem("Silver Amulet", win)
+    game_state.inventory.getItem("Lamp Oil", win)
+    game_state.inventory.getItem("Lamp Oil", win)
+    game_state.inventory.getItem("Rope", win)
+    game_state.inventory.getItem("Bomb", win)
+    game_state.inventory.getItem("Bomb", win)
+    game_state.getRuby(9001, win)
